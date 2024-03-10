f2f953d5d5327eb8ea5109a74b611ada54197599
Mariohjr94 <mariohjr94@gmail.com>
2024-03-10 00:12:59 -0500

    adding player, still need to be scale properly

--- client/main.js
+++ client/main.js
@@ -1,5 +1,6 @@
 import "./style.css";
 import Phaser from "phaser";
+import Player from "./js/classes/Player.js";
 
 const sizes = {
   width: 1000,
@@ -19,8 +20,8 @@ class GameScene extends Phaser.Scene {
     this.load.image("tiles", "/public/game-assets/map/battlefield.png");
     this.load.spritesheet(
       "player",
-      "/game-assets/Archers-Character/Archers/Archer-1.png",
-      { frameWidth: 12, frameHeight: 12 }
+      "/public/game-assets/Archers-Character/Archers/Archer-1.png",
+      { frameWidth: 64, frameHeight: 48 }
     );
   }
 
@@ -44,13 +45,15 @@ class GameScene extends Phaser.Scene {
     const collisionLayer = map.createLayer("collision", tileset, 0, 0);
     collisionLayer.setScale(scaleFactor);
     collisionLayer.setCollisionByExclusion([-1]);
-    collisionLayer.setAlpha(0);
+    collisionLayer.setAlpha(0); // makes layer invisible
 
     //player
-    const player = this.physics.add.sprite(100, 100, "player");
-    this.physics.world.enable(player);
-    this.physics.add.collider(player, collisionLayer);
-    player.setCollideWorldBounds(true);
+    this.player = new Player(this, 100, 100);
+    this.player.setOrigin(0.5, 0.5);
+    this.player.setScale(1.5);
+    this.physics.world.enable(this.player);
+    // Add collision between player and collision layer
+    this.physics.add.collider(this.player, collisionLayer);
   }
 
   update() {}
@@ -60,6 +63,7 @@ const config = {
   type: Phaser.WEBGL,
   width: sizes.width,
   height: sizes.height,
+  mode: Phaser.Scale.FIT,
   canvas: gameCanvas,
   physics: {
     default: "arcade",
